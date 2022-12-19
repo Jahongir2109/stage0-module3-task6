d1a08ac4455f29a4e8145e0de64ff0cae8b01e28
Jahongir2109 <jahongirachilov004@gmail.com>
2022-12-19 17:31:37 +0500

    fixed with math termins

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        if((numberToBeRounded-(int)numberToBeRounded)>0.5) {
+        if((numberToBeRounded-(int)numberToBeRounded)>=0.5) {
             System.out.println((int)numberToBeRounded+1);
         }else {
             System.out.println((int)numberToBeRounded);
